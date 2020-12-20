0096ae9a0eb252e5790bc56dc6f2344b01b946bf
Winter <oncwnuC1JxstoaDOLfQSO6CBiTns@git.weixin.qq.com>
2020-12-21 02:56:45 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+
+  onLoad: function () {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+  },
+
+  onGetOpenid: function () {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+
+})
